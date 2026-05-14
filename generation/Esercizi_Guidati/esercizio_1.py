2dadc29d5efdbddaec7c5e8f69b415239bd41148
MaximoMephisto <maxximo.sorondo@gmail.com>
2026-05-14 17:00:09 +0200

    ultimo_aggiornamento

---
+++ generation/Esercizi_Guidati/esercizio_1.py
@@ -0,0 +1,22 @@
+n = 15
+if n < 8:
+    print("The number is less than 8")
+if n > 5:
+    print("The number is greater than 5")
+
+# Quali messaggi vi aspettate che vengano stampati?
+## "The number is greater than 5"
+
+# Provate ad eseguire per verificare la vostra ipotesi.
+## Just
+
+# Cambiate il valore assegnato alla variabile n nella prima riga, cercando di ottenere
+# come risultato che venga fatta solo la prima stampa e non la seconda.
+## 4
+
+# Cambiate il valore assegnato alla variabile n in modo tale che entrambe le stampe
+# siano eseguite.
+## 7
+
+# In tutti i casi, mandate il codice in esecuzione per verificare che le vostre ipotesi
+# siano corrette.
