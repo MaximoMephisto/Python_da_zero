358e23b5bf4cc1619b5cc73417c9d2e5bb1e5029
MaximoMephisto <maxximo.sorondo@gmail.com>
2026-05-19 09:25:20 +0200

    'changes in bonus'

--- generation/Esercizio_Bonus/esercizio_due.py
+++ generation/Esercizio_Bonus/esercizio_due.py
@@ -33,32 +33,31 @@ orario = input("Quante ore vuole noleggiare? (Es. 1:30): ")
 ore, minuti = orario.split(":")
 ore = int(ore)
 minuti = int(minuti)
+
 if ore < 0 or ore > 23:
     print("Errore, ora non giusta.")
-    exit()
+elif minuti < 0 or minuti > 59:
+    print("Errore, minuti non giusti.")
 else:
-    if minuti < 0 or minuti > 59:
-        print("Errore, minuti non giusti.")
-        exit()
-    else:
-        minuti_a_ora = minuti / 60
-        tot_orario = minuti_a_ora + ore
+    minuti_a_ora = minuti / 60
+    tot_orario = minuti_a_ora + ore
     
-opt_1 = input("Desidera aggiungere dei caschi? (Es. S/n): ")
-if opt_1.lower() == "s" or opt_1.lower == "si":
-    caschi = int(input("Inserisci il numero di caschi: "))
-    if caschi < 0:
-        print("Error, inserire numero valido.")
-    else:
-        accessori = caschi * 2
-elif opt_1.lower() == "n" or opt_1.lower == "no":
-    caschi = 0
-    accessori = 0
-else :
-    print("Errore, caratteri non accetatti.")
+    opt_1 = input("Desidera aggiungere dei caschi? (Es. S/n): ")
+    if opt_1.lower() == "s" or opt_1.lower == "si":
+        caschi = int(input("Inserisci il numero di caschi: "))
+        if caschi < 0:
+            print("Error, inserire numero valido.")
+        else:
+            accessori = caschi * 2
+    elif opt_1.lower() == "n" or opt_1.lower == "no":
+        caschi = 0
+        accessori = 0
+    else :
+        print("Errore, caratteri non accetatti.")
 
 tot_noleggio = (prezzo_bici * tot_orario) + accessori
 
+print("=================")
 print("= Noleggio Bici =")
 print(f"Tipo: {nome_bici}")
 print(f"Tempo di noleggio: {ore}:{minuti}h")
