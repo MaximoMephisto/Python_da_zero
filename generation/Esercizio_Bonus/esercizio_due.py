5c222f48ce1627d0927fd19346ec765af082161f
MaximoMephisto <maxximo.sorondo@gmail.com>
2026-05-19 10:08:52 +0200

    Remove exit() dal esercizio_due

--- generation/Esercizio_Bonus/esercizio_due.py
+++ generation/Esercizio_Bonus/esercizio_due.py
@@ -30,49 +30,51 @@ else:
         nome_bici = "Bicicletta elettrica"
         prezzo_bici = 8
 
-    orario = input("Quante ore vuole noleggiare? (Es. 1:30): ")
-    ore, minuti = orario.split(":")
-    ore = int(ore)
-    minuti = int(minuti)
+    if opt == 1 or opt == 2 or opt == 3:
+        orario = input("Quante ore vuole noleggiare? (Es. 1:30): ")
+        ore, minuti = orario.split(":")
+        ore = int(ore)
+        minuti = int(minuti)
 
-    if ore < 0 or ore > 23:
-        print("Errore, ora non giusta.")
-    elif minuti < 0 or minuti > 59:
-        print("Errore, minuti non giusti.")
-    else:
-        minuti_a_ora = minuti / 60
-        tot_orario = minuti_a_ora + ore
+        if ore < 0 or ore > 23:
+            print("Errore, ora non giusta.")
+        elif minuti < 0 or minuti > 59:
+            print("Errore, minuti non giusti.")
+        else:
+            minuti_a_ora = minuti / 60
+            tot_orario = minuti_a_ora + ore
+            
+            opt_1 = input("Desidera aggiungere dei caschi? (Es. S/n): ")
+            if opt_1.lower() == "s" or opt_1.lower() == "si":
+                caschi = int(input("Inserisci il numero di caschi: "))
+                if caschi < 0:
+                    print("Error, inserire numero valido.")
+                else:
+                    accessori = caschi * 2
+            elif opt_1.lower() == "n" or opt_1.lower == "no":
+                caschi = 0
+                accessori = 0
+            else :
+                print("Errore, caratteri non accetatti.")
 
-        opt_1 = input("Desidera aggiungere dei caschi? (Es. S/n): ")
-        if opt_1.lower() == "s" or opt_1.lower == "si":
-            caschi = int(input("Inserisci il numero di caschi: "))
-            if caschi < 0:
-                print("Error, inserire numero valido.")
-            else:
-                accessori = caschi * 2
-        elif opt_1.lower() == "n" or opt_1.lower == "no":
-            caschi = 0
-            accessori = 0
-        else :
-            print("Errore, caratteri non accetatti.")
+            if (opt_1 == "s" or opt_1 == "si" or opt_1 == "n" or opt_1 == "no") and (caschi >= 0):
+                tot_noleggio = (prezzo_bici * tot_orario) + accessori
 
-        tot_noleggio = (prezzo_bici * tot_orario) + accessori
-
-        print("=================")
-        print("= Noleggio Bici =")
-        print(f"Tipo: {nome_bici}")
-        print(f"Tempo di noleggio: {ore}:{minuti}h")
-        print(f"Accesori aggiunti: {caschi}")
-        if stagione == 1:
-            percentuale = (round(tot_noleggio) / 100) * 30
-            tot_noleggio = tot_noleggio + percentuale
-            print("Aumento del 30% per alta stagione.")
-        if tot_orario > 8:
-            percentuale = (round(tot_noleggio) / 100) * 15
-            tot_noleggio = tot_noleggio - percentuale
-            print("Sconto applicato del 15% per durata del noleggio.")
-        if tot_orario < 2:
-            supplemento = 4
-            tot_noleggio = tot_noleggio + supplemento
-            print("4£ di supplemento fisso per gestione e durata del noleggio..")
-        print(f"Prezzo totale del noleggio: {round(tot_noleggio, 2)}£")
+                print("=================")
+                print("= Noleggio Bici =")
+                print(f"Tipo: {nome_bici}")
+                print(f"Tempo di noleggio: {ore}:{minuti}h")
+                print(f"Accesori aggiunti: {caschi}")
+                if stagione == 1:
+                    percentuale = (round(tot_noleggio) / 100) * 30
+                    tot_noleggio = tot_noleggio + percentuale
+                    print("Aumento del 30% per alta stagione.")
+                if tot_orario > 8:
+                    percentuale = (round(tot_noleggio) / 100) * 15
+                    tot_noleggio = tot_noleggio - percentuale
+                    print("Sconto applicato del 15% per durata del noleggio.")
+                if tot_orario < 2:
+                    supplemento = 4
+                    tot_noleggio = tot_noleggio + supplemento
+                    print("4£ di supplemento fisso per gestione e durata del noleggio..")
+                print(f"Prezzo totale del noleggio: {round(tot_noleggio, 2)}£")
