e3600115c11f5d415561e45555da857c50016202
MaximoMephisto <maxximo.sorondo@gmail.com>
2026-06-01 10:33:23 +0200

    (●'◡'●)

--- generation/stringe/gestione_biblioteca_bonus.py
+++ generation/stringe/gestione_biblioteca_bonus.py
@@ -1,20 +1,23 @@
-#dati_grezzi = " Il nome della rosa;Umberto Eco;1980;disponibile |1984;George Orwell;1949;prestato| La coscienza di Zeno ;italo svevo;1923;DISPONIBILE|Il deserto dei tartari;Dino Buzzati;1940;prestato |1984;George Orwell;1949;prestato"
-libri = []
+# Lista obligatoria
+libri = [] 
+# Liste di suporto
 titoli = []
 autori = []
 anni = []
 stato = []
-
+# Primo ciclo per ripetere richieste di libri
 while True:
+    # Secondo ciclo per verificare i dati inseriti
     sequenza = 1
     while sequenza == 1:
         dati_grezzi = input("Inserisci libro (titolo;autore;anno;stato): ")
-        
+        # lower() per passare tutte a minoscola, split() per separare in una lista dipendendo dal valore dentro (';')
         dati_grezzi = dati_grezzi.lower()
         verifica_dati = dati_grezzi.split(';')
         
         if len(verifica_dati) != 4:
             print("Errore, ci devono essere 4 dati inseriti.")
+            # Ricomincia il cilo dal inizio senza continuare alle altre condizioni
             continue
 
         if not verifica_dati[2].isdigit():
@@ -24,14 +27,17 @@ while True:
         if verifica_dati[3] != "disponibile" and verifica_dati[3] != "prestato":
             print("Errore, inserire uno stato valido.")
             continue
-        
+        # Stop ciclo
         sequenza = 0     
             
     # Pulizia
+    # Separiamo i dati in lista e si remplazano i '|' per una virgola con un salto di linea
     dati_grezzi = dati_grezzi.strip().replace("|", ",\n")
+    # Separiamo ogni libro per ogni virgola e salto di linea
     dati_grezzi = dati_grezzi.split(',\n')
 
     for dati in dati_grezzi:
+        # Separiamo ogni dato del libro in una lista e si aggiungono alle liste di suporto
         dati = dati.strip().split(';')
         titoli.append(dati[0])
         
@@ -52,28 +58,29 @@ while True:
     print("==========")
 
     # print(sorted(titoli))
-    ########################### asdasdd
+    ########################### 
     ###########################
+    # Facciamo una copia de la lista per non modificare la originale
     copia_titoli = list(titoli)
-    titoli_sistemati = []
     
-    for titolo in copia_titoli:
-        
-        for elem in titolo:  
-            minore = ord(titolo[0])
-            if elem < minore:
-                minore = elem
-        
-        titoli_sistemati.append(minore)
-        copia_titoli.remove(minore)
-    
-    print(titoli_sistemati)   
+    num = len(copia_titoli)
+    for i in range(num):
+        cambio = False
+        for j in range(0, num - i -1):
+            if copia_titoli[j] > copia_titoli[j + 1]: # Se il indice e maggiore al inidice che procede
+                #     b                a            ->       a                    b
+                copia_titoli[j], copia_titoli[j + 1] = copia_titoli[j + 1], copia_titoli[j] 
+                cambio = True # C'e stato cambio quindi continua a verificare
+        if not cambio:
+            break
+
+    print(f"TITOLI SORT: {copia_titoli}")
     ###########################
     ###########################
         
     print("==========")
 
-
+    # Prendiamo il anno con valore piu grande (nuovo) e quello con valore piu piccolo (vecchio)
     for titolo in titoli:
         for anno in anni:
             vecchio = min(anni)
@@ -89,6 +96,7 @@ while True:
 
                 break
 
+    # Facciamo un conto di quanti libri sono disponibili e quanti prestati
     qta_disponibile = 0
     qta_non_disponibile = 0
     for elem in stato:
@@ -117,7 +125,7 @@ while True:
                 print(libro)
     print("==========")
                 
-
+    # Facciamo un print dei libri che ci sono tra questi anni (estremi commpresi)
     anno_inizio = 1940
     anno_fine = 1980
 
@@ -143,7 +151,8 @@ while True:
 
     print(f"Libro duplicato: {titolo_duplicato} di {autore_duplicato}")
     print("==========")
-            
+    
+    # Facciamo una nuova lista con i libri senza duplicati
     for libro in libri:
         if libro not in libri_non_duplicati:
             libri_non_duplicati.append(libro)
@@ -156,6 +165,7 @@ while True:
         
 
     # Statistiche sulle stringhe
+    # Si crea una lista con i titoli senza i duplicati per calcolare la media della lunghezza (carattere) dei titoli
     titoli_non_duplicati = []
     for titolo in titoli:
         if titolo not in titoli_non_duplicati:
@@ -171,7 +181,7 @@ while True:
     print(f"lunghezza media titoli: {media}")
     print("==========") 
 
-
+    # Facciamo print di quello piu lungo
     titolo_piu_lungo = ""
     lunghezza_titolo = 0
     for titolo in titoli_non_duplicati:
@@ -189,18 +199,20 @@ while True:
     for titolo in titoli_non_duplicati:
         for lettera in titolo:
             if lettera in vocali:
+                # per trovare l'indice della vocale trovata nei titoli e sommare allo stesso indice di un altra lista
                 i = vocali.index(lettera)
                 conteggi[i] += 1
 
     for vocale in vocali:
-        coppie = []
+        coppie = [] # Dentro il for per ritornare a valore zero una volta inseriti i dati nell altra lista
         if vocale not in coppie:
             coppie.append(vocale)
-            i = vocali.index(vocale)
+            i = vocali.index(vocale) # Indice della vocale
             for num in conteggi:
-                if conteggi.index(num) == i:
+                if conteggi.index(num) == i: # Stesso indice che la vocale
                     coppie.append(num)
         
+        # Si salvano i dati nella lista delle coppie senza ripetergli
         if coppie not in lista_coppie:
             lista_coppie.append(coppie)
                 
@@ -211,11 +223,12 @@ while True:
     titolo_pulito = []
 
     for titolo in titoli_non_duplicati:
-        
+        # Facciamo tutto minoscolo e si aggiungie uno spazio prima del inizio per far stare tutte le parole da saltare nella stessa posizione
         titolo = " " + titolo.lower()
         
         for salto in da_saltare:
             if salto in titolo:
+                # Si cambiano le parole per uno spazio vuoto
                 titolo = titolo.replace(f" {salto} " , " ")
             
         titolo_pulito.append(titolo.upper())
@@ -224,12 +237,13 @@ while True:
     print("==========") 
 
     lettere = []
-
+    # Prende l'indice 0 del titolo separato in lista
     for titolo in titolo_pulito:    
         prime_lettere = [lettera[0] for lettera in titolo.split()]
-        parola = "".join(prime_lettere)
-        lettere.append(parola)
+        parola = "".join(prime_lettere) # Definiamo la variabile parola con il valore della prima lettera
+        lettere.append(parola) # Si aggiungono queste lettere a la lista
 
+    # Per ogni elemento della lista lettere fare print con lo stesso indice di titoli e lettera
     for titolo in titoli_non_duplicati:
         for elem in lettere:
             if titoli_non_duplicati.index(titolo) == lettere.index(elem):
@@ -237,6 +251,7 @@ while True:
 
     print("==========") 
 
+    # Un output sistemato
     output = ""
 
     for i in range(len(titoli_non_duplicati)):
@@ -248,6 +263,7 @@ while True:
 
     print(output)
     
+    # Si chiede se volgiono aggiungere un altro libro alla lista per non far finire il codice con un solo valore
     opt = input("Vuoi aggiungere un altro libro? (S/n): ")
     while opt.lower() != "s" and opt.lower() != "n":
         print("Errore. Option non giusta.")
